afdd8e6bf3a4922e6d263f9239ee3fb0fcf3747a
minami-choi <cmina2846@gmail.com>
2018-04-26 22:17:31 +0900

    master-react

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+
+class ReturnTypes extends Component {
+  render() {
+    return(
+      <Fragment>
+      <header></header>
+      <div></div>
+      <footer></footer>
+      </Fragment>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return <div className="App"/>;
+  }
+}
+
+export default App;
