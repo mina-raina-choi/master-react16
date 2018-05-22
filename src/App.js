0bb056fdacb743e8f5d325318dd6197e1cf13ea5
minami-choi <cmina2846@gmail.com>
2018-05-22 23:43:10 +0900

    react 16의 에러캐치에 대한 새로운 방법들

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
 import React, { Component, Fragment } from 'react';
+import { createPortal } from "react-dom";
+
+const BoundaryHOC = ProtectedComponent => 
+  class BoundaryHOC extends Component {
+    state = {
+      hasError: false
+    }
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true
+      })
+    }
+    render() {
+      const { hasError } = this.state;
+      if(hasError) {
+        return < ErrorFallback/>
+      } else {
+        return <ProtectedComponent/>
+      }
+    }
+  }
+
+class ErrorMaker extends Component {
+    state = {
+      friends: ['jisu', 'daal', 'kneeprayer']
+    }
+    componentDidMount = () => {
+      setTimeout(() => {
+        this.setState({
+          friends: undefined
+        })
+      }, 2000);
+    }
+    render() {
+      const { friends } = this.state;
+      return friends.map(friend => ` ${friend} `)
+    }
+  }
+
+const PErrorMaker = BoundaryHOC(ErrorMaker)
+
+const ErrorFallback = () => " Sorry something went wrong"
+
+class Portals extends Component {
+  render() {
+    return createPortal(
+      <Message />, document.getElementById("touchme")
+    )
+  }
+}
+
+const PPortals = BoundaryHOC(Portals)
+
+const Message = () => "Just touched it!";
 
 class ReturnTypes extends Component {
   render() {
-    return(
-      <Fragment>
-      <header></header>
-      <div></div>
-      <footer></footer>
-      </Fragment>
-    )
+    return "hello"
   }
 }
 
 class App extends Component {
   render() {
-    return <div className="App"/>;
+    return (<Fragment>
+      <ReturnTypes />
+      <PPortals />
+      <PErrorMaker />
+    </Fragment>);
   }
 }
 
-export default App;
+
+
+export default BoundaryHOC(App);
